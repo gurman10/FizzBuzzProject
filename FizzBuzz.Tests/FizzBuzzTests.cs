6ed22a1fd54778bd330fe7eaf7817b5e5951a2df
gurman10 <gurmandeepthind@gmail.com>
2022-09-12 00:08:05 -0700

    Initial commit

---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests;
+
+[TestFixture(Author = "Gurman Deep Singh")]
+public class FizzBuzzTests
+{ 
+    [Test]
+    public void When_Mutiple_Of_3_Expect_Fizz()
+    {
+        string expected = "Fizz";
+        var actual = Program.FizzBuzzIt(9);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void When_Multiple_Of_5_Expect_Buzz()
+    {
+        string expected = "Buzz";
+        var actual = Program.FizzBuzzIt(25);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void When_Multiple_Of_Both_3_And_5_Expect_FizzBuzz()
+    {
+        string expected = "FizzBuzz";
+        var actual = Program.FizzBuzzIt(15);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void When_Not_A_Multiple_Of_3_Or_5_Expect_Same_Number()
+    {
+        string expected = "26";
+        var actual = Program.FizzBuzzIt(26);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Output_Of_1_To_100()
+    {
+        ICollection expected = new ArrayList {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz", "31", "32", "Fizz", "34", "Buzz", "Fizz", "37", "38", "Fizz", "Buzz", "41", "Fizz", "43", "44", "FizzBuzz", "46", "47", "Fizz", "49", "Buzz", "Fizz", "52", "53", "Fizz", "Buzz", "56", "Fizz", "58", "59", "FizzBuzz", "61", "62", "Fizz", "64", "Buzz", "Fizz", "67", "68", "Fizz", "Buzz", "71", "Fizz", "73", "74", "FizzBuzz", "76", "77", "Fizz", "79", "Buzz", "Fizz", "82", "83", "Fizz", "Buzz", "86", "Fizz", "88", "89", "FizzBuzz", "91", "92", "Fizz", "94", "Buzz", "Fizz", "97", "98", "Fizz", "Buzz"};
+        var actual = Enumerable.Range(1, 100).Select(x => Program.FizzBuzzIt(x)).ToList();
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,32 @@
+FizzBuzz README
+_ _ _ _ _ _ _ _ _ 
+
+
+Requirements:
+
+In order to run the solution you will need Visual Studio Community 2022.
+The latest version of Visual Studio can be downloaded from the link below.
+
+https://visualstudio.microsoft.com/downloads/
+
+Instructions to install necessary components of Visual Studio on Windows 10.
+
+1. Download the community version by clicking on Free download.
+2. Once download is complete VisualStudioSetup.exe will appear in the location you specified during the download.
+3. Right click on VisualStudioSetup.exe and Run as Administrator, enter the admin credentials if prompted otherwise click "Yes" to proceed.
+4. In the Visual Studio Installer Click "Continue".
+5. Click the box next to ".NET desktop development" and then click the "Install" button in bottom right. Wait for the installation to complete.
+6. After installation is complete, follow the onscreen instructions to Start Visual Studio.
+
+Note: The installation process is similar for MacOS. Make sure to choose appropriate installer for it.
+
+Instruction to run the FizzBuzz program and run the tests on Windows 10 using Visual Studio.
+
+1. Download the FizzBuzzProject folder on your computer.
+2. Navigate to the directory where the FizzBuzzProject is downloaded.
+3. Navigate to FizzBuzzProject > FIzzBuzz. Open "FizzBuzz.sln" file with Microsoft Visual Studio 2022.
+4. To run the FizzBuzz program press "F5" key or click on Green button followed by "FizzBuzz" in the top middle. Console window will appear with the program output.
+5. To run the test on this program click on Test tab and choose "Run all Test". Test Explorer window will pop up with start the Build. Once the Build is complete click the first button under Test explorer in the top left corner.
+6. The Explorer displays all the tests, the tests that passed are shown with the Green checkmark while failed tests if any will be displayed with a red cross.
+
+
